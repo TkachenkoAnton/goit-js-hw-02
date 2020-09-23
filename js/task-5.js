4bd3fcda48417c3ffccb0db1a823a485ef01d6e4
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-23 20:15:40 +0300

    добавляет решение 5 задачи

--- js/task-5.js
+++ js/task-5.js
@@ -1,9 +1,18 @@
-const checkForSpam = function (str) {};
+const checkForSpam = function (message) {
+  if (message.toLowerCase().split(" ").includes("sale")) {
+    message = true;
+  } else if (message.toLowerCase().split(" ").includes("[spam]")) {
+    message = true;
+  } else {
+    message = false;
+  }
+  return message;
+};
 
-//console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam("Latest technology news")); // false
 
-//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
 
-//console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam("Get best sale offers now!")); // true
 
-//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(checkForSpam("[SPAM] spam How to earn fast money?")); // true
