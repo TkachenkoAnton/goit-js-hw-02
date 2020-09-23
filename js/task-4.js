1b0077116e5cc80540e7614bff30d4d7c29a3e3b
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-23 18:13:33 +0300

    добавляет условия 4 задачи

---
+++ js/task-4.js
@@ -0,0 +1,10 @@
+function formatString(string, maxLength = 40) {}
+
+//console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+// 'Curabitur ligula sapien, tincidunt non.'
+
+//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+// 'Vestibulum facilisis, purus nec pulvinar...'
+
+//console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+// 'Vestibulum facilisis, purus ne...'
