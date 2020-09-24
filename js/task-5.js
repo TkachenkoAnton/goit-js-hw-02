167c941379d77d368a4ca8ca16a59921d3329bec
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-24 10:04:02 +0300

    добавляет варианты решения задачи 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,13 +1,26 @@
+// Мое решение
+
 const checkForSpam = function (message) {
-  if (message.toLowerCase().split(" ").includes("sale")) {
+  if (message.toLowerCase().includes("sale")) {
     message = true;
-  } else if (message.toLowerCase().split(" ").includes("[spam]")) {
+  } else if (message.toLowerCase().includes("spam")) {
     message = true;
   } else {
     message = false;
   }
+
   return message;
 };
+
+// Решение ментора GoIT
+
+// function checkForSpam(str) {
+
+//   return str.toLowerCase().includes("spam") ||
+//     str.toLowerCase().includes("sale")
+//     ? true
+//     : false;
+// }
 
 console.log(checkForSpam("Latest technology news")); // false
 
