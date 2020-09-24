18fa01a2c5e4ce0cb62fb6ab5721235bd90189a9
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-24 11:17:51 +0300

    вносит изменеия в решение задачи 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,5 @@
 const mapArray = function (array) {
-  const numbers = new Array(array.length);
+  const numbers = new Array(array);
   for (let i = 0; i < array.length; i += 1) {}
 
   return numbers;
