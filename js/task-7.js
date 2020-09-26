938cc8dcc86a247b3f690ddb8b0163e12d42d375
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-27 01:04:58 +0300

    частично обавляет решение 7 задачи

--- js/task-7.js
+++ js/task-7.js
@@ -1,22 +1,25 @@
 const filterArray = function (array) {
   const numbers = [];
+  numbers.push(array);
   for (let i = 0; i < array.length; i += 1) {
-    // Write code under this line
+    if (Number.isFinite(array[i]) === true) {
+      numbers[i] = array[i];
+    }
   }
   return numbers;
 };
 
-//console.log(filterArray([-2, 0, 2]));
+console.log(filterArray([-2, 0, 2]));
 // [-2, 0, 2]
 
-//console.log(filterArray([1, NaN, Infinity]));
+console.log(filterArray([1, NaN, Infinity]));
 // [1]
 
-//console.log(filterArray([0, -0, 100, '100']));
+console.log(filterArray([0, -0, 100, "100"]));
 // [0, 0, 100]
 
-//console.log(filterArray([undefined, false, null, [], 1]));
+// console.log(filterArray([undefined, false, null, [], 1]));
 // [1]
 
-//console.log(filterArray([{}, () => {}, 2]));
+// console.log(filterArray([{}, () => {}, 2]));
 // [2]
