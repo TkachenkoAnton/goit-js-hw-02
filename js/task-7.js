014a3aa9d6529b83417f1b6919e0ca537d5b5807
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-26 17:38:00 +0300

    добавляет условие 7 задачи

---
+++ js/task-7.js
@@ -0,0 +1,22 @@
+const filterArray = function (array) {
+  const numbers = [];
+  for (let i = 0; i < array.length; i += 1) {
+    // Write code under this line
+  }
+  return numbers;
+};
+
+//console.log(filterArray([-2, 0, 2]));
+// [-2, 0, 2]
+
+//console.log(filterArray([1, NaN, Infinity]));
+// [1]
+
+//console.log(filterArray([0, -0, 100, '100']));
+// [0, 0, 100]
+
+//console.log(filterArray([undefined, false, null, [], 1]));
+// [1]
+
+//console.log(filterArray([{}, () => {}, 2]));
+// [2]
