a1148541f68a2952f66c2eb3f3df3ce65097d6a3
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-26 17:30:50 +0300

    добавляет решение 6 задачи

--- js/task-6.js
+++ js/task-6.js
@@ -1,6 +1,9 @@
 const mapArray = function (array) {
-  const numbers = new Array(array);
-  for (let i = 0; i < array.length; i += 1) {}
+  const numbers = new Array(array.length);
+
+  for (let i = 0; i < array.length; i += 1) {
+    numbers.splice(i, 1, array[i] * 10);
+  }
 
   return numbers;
 };
@@ -8,5 +11,5 @@ const mapArray = function (array) {
 console.log(mapArray([-2, 0, 2]));
 // [-20, 0, 20]
 
-//console.log(mapArray([-2.5, 0, 2.5]));
+console.log(mapArray([-2.5, 0, 2.5]));
 // [-25, 0, 25]
