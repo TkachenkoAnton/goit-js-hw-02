a24e88c3f98e6e1a35acb6b942ec1116518fb9f8
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-27 10:59:30 +0300

    Добавляет условия 8 задачи

---
+++ js/task-8.js
@@ -0,0 +1,14 @@
+const reduceArray = function (array) {
+  let total = 0;
+
+  return total;
+};
+
+//console.log(reduceArray([1, 2, 3]));
+// 6
+
+//console.log(reduceArray([-2, 0, 2]));
+// 0
+
+//console.log(reduceArray([1, 2, 2.5]));
+// 5.5
