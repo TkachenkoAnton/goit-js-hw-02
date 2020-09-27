df2f3e8e19f8ae2c02b435999ce69a5edeb24d5d
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-27 11:02:16 +0300

    добавляет решение 8 задачи

--- js/task-8.js
+++ js/task-8.js
@@ -1,14 +1,17 @@
 const reduceArray = function (array) {
   let total = 0;
-
+  for (let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    total += element;
+  }
   return total;
 };
 
-//console.log(reduceArray([1, 2, 3]));
+console.log(reduceArray([1, 2, 3]));
 // 6
 
-//console.log(reduceArray([-2, 0, 2]));
+console.log(reduceArray([-2, 0, 2]));
 // 0
 
-//console.log(reduceArray([1, 2, 2.5]));
+console.log(reduceArray([1, 2, 2.5]));
 // 5.5
