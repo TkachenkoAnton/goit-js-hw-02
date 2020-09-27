fba6f74bcadcac5bd3506d09968468baf9a28c96
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-27 11:08:21 +0300

    добавляет условие 9 задачи

---
+++ js/task-9.js
@@ -0,0 +1,25 @@
+const isLoginValid = function (login, min = 4, max = 16) {};
+
+const isLoginUnique = function (allLogins, login) {};
+
+const addLogin = function (allLogins, login) {
+  "use strict";
+  const SUCCESS = "Логин успешно добавлен!";
+  const REFUSAL = "Такой логин уже используется!";
+  const ERROR = "Ошибка! Логин должен быть размером от 4 до 16 символов";
+  let message;
+};
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+// console.log(addLogin(logins, 'Ajax'));
+// 'Логин успешно добавлен!'
+
+// console.log(addLogin(logins, 'robotGoogles'));
+// 'Такой логин уже используется!'
+
+// console.log(addLogin(logins, 'Zod'));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+// console.log(addLogin(logins, 'jqueryisextremelyfast'));
+// 'Ошибка! Логин должен быть от 4 до 16 символов'
