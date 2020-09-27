248028070e51b919571c6eb27ae01a9a72813e2a
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-09-27 11:05:47 +0300

    Добавляет решение 8 задачи

--- js/task-8.js
+++ js/task-8.js
@@ -1,9 +1,16 @@
 const reduceArray = function (array) {
+  // Объявить переменную для подсчета суммы элементов массива
   let total = 0;
+
+  // С помощью цикла фор перебрать массив
   for (let i = 0; i < array.length; i += 1) {
     const element = array[i];
+
+    // Посчитать сумму всех элементов массива
     total += element;
   }
+
+  // Вернуть значение суммы элементов массив
   return total;
 };
 
